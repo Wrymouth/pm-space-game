a95c3157b856f334a3f20d5b2b8671d2b7017a01
Wrymouth <jacco.potters@outlook.com>
2023-07-01 00:06:17 +0200

    setup texture pan

--- src/world/area_spc/spc_00/main.c
+++ src/world/area_spc/spc_00/main.c
@@ -1,5 +1,6 @@
 #include "spc_00.h"
-#include "../src/world/common/atomic/TexturePan.inc.c"
+#include "world/common/atomic/TexturePan.inc.c"
+
 
 
 s32 N(map_init)(void) {
@@ -75,6 +76,15 @@ EvtScript N(GameLoop) = {
 };
 
 EvtScript N(SetupTexturePan) = {
+    // EVT_CALL(EnableTexPanning, Model_ScrollingBG, TRUE)
+    EVT_CALL(SetTexPanner, Model_ScrollingBG, TEX_PANNER_0)
+    EVT_THREAD
+        TEX_PAN_PARAMS_ID(TEX_PANNER_0)
+        TEX_PAN_PARAMS_STEP(2000,2000,2000,0)
+        TEX_PAN_PARAMS_FREQ(    0,    0,    0,    0)
+        TEX_PAN_PARAMS_INIT(    0,    0,    0,    0)
+        EVT_EXEC(N(EVS_UpdateTexturePan))
+    EVT_END_THREAD
     EVT_RETURN
     EVT_END  
 };
